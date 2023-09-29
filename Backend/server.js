886ec1150d997cffe49aeec04232450ed8008fdf
Pranjal Tyagi <pranjaltyagi@gmail.com>
2023-09-30 00:48:54 +0530

    ready for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,38 @@
-const dotenv=require('dotenv')
-dotenv.config(); //importing the environment variables
-const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config(); // Importing the environment variables
+const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
-const cors =  require('cors');
+const mongoose = require('mongoose');
+const cors = require('cors');
 app.use(
     cors({
-        origin:"*",
+        origin: "*",
     })
-)
+);
+const uri = "mongodb+srv://pranjal:pranjal@cluster0.9b1nmhi.mongodb.net/?retryWrites=true&w=majority";
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '50mb' })); // Adjust the limit as needed
 
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true })
-const db= mongoose.connection;
-db.on('error',(error)=> console.error(error));
-app.use(express.json())
+mongoose.connect(uri, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+app.use(express.json());
 
-//setting up the authRoutes(Login,SignUp)
-const userRoutes = require('./routes/authRoutes')
-app.use('/api',userRoutes);
+// Setting up the authRoutes (Login, SignUp)
+const userRoutes = require('./routes/authRoutes');
+app.use('/api', userRoutes);
 
 const uploadRoutes = require('./routes/uploadroutes');
-// const upload = require('./controllers/uploadcontroller');
-app.use('/upload',uploadRoutes);
+app.use('/upload', uploadRoutes);
 
-app.get('/',(req,res)=>{
-    res.send("<h1>Hello</h1>")
+app.get('/', (req, res) => {
+    res.send("<h1>Hello</h1>");
     res.end();
-})
-const port= process.env.port|| 3000;
-console.log(process.env.port)
-app.listen(process.env.port,()=>{
-})
+});
 
+const port = process.env.PORT || 3000;
+console.log(process.env.PORT);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
